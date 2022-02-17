c450f2eb0db755c3181b8f5154f437b7fe6ab32b
Samirun-Shuvo <samirunshuvo@gmail.com>
2022-02-17 19:17:19 +0600

    Error Message Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//function for get all input value
 function getInputValueIncomeExpense(amountof) {
     const inputTextValue = document.getElementById(amountof + '-input');
     const inputAmount = parseInt(inputTextValue.value);
@@ -5,13 +6,15 @@ function getInputValueIncomeExpense(amountof) {
         inputTextValue.value = '';
         return inputAmount;
     } else {
-        alert('Input Amout Will Be Positive');
+        alert('Opps..! You have to give positive amount');
+        inputTextValue.value = '';
+        return getInputValueIncomeExpense();
     }
-
 }
+
 //clicked calculate button
 document.getElementById('calculate-button').addEventListener('click', function () {
-    //get input field value
+    //get input field value calling function
     var incomeInputValue = getInputValueIncomeExpense('income');
     var foodInputValue = getInputValueIncomeExpense('food');
     var rentInputValue = getInputValueIncomeExpense('rent');
@@ -24,26 +27,28 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const balanceTotalText = document.getElementById('balance-total');
     const balanceTotalValue = parseInt(balanceTotalText.innerText);
 
-    //calculation balance and set to html
+    //calculation balance and set to html with condition
     const expenses = foodInputValue + rentInputValue + clothesInputValue;
     if (incomeInputValue >= expenses) {
         expanceTotalText.innerText = expenses;
         const balance = incomeInputValue - expenses;
         balanceTotalText.innerText = balance;
+        var balanceMessage = document.getElementById('balance-message');
+        balanceMessage.style.display = 'none';
+
     } else {
-        alert('Income is low than Expence')
+        var balanceMessage = document.getElementById('balance-message');
+        balanceMessage.style.display = 'block';
         expanceTotalText.innerText = '00';
         balanceTotalText.innerText = '00';
     }
-
 })
 
 
 //clicked save button
 document.getElementById('save-button').addEventListener('click', function () {
-    //get input field value
-    const saveInputParsentValue = getInputValueIncomeExpense('save')
-
+    //get input field value calling function
+    const saveInputParsentValue = getInputValueIncomeExpense('save');
 
     //get html text-field value
     const balanceTotalText = document.getElementById('balance-total');
@@ -55,18 +60,16 @@ document.getElementById('save-button').addEventListener('click', function () {
     const remainingBalanceText = document.getElementById('remaining-balance');
     const remainingBalanceValue = parseInt(remainingBalanceText.innerText);
 
-    //calculation remaining balance and set to html
-
-    if (balanceTotalValue >= 0 && balanceTotalValue >= saveInputParsentValue) {
+    //calculation remaining balance and set to html with condition
+    if (balanceTotalValue > 0 && saveInputParsentValue <= 100) {
         const savingAmount = (balanceTotalValue * saveInputParsentValue) / 100;
         saveAmountText.innerText = savingAmount;
-
         const remainingBalance = balanceTotalValue - savingAmount;
         remainingBalanceText.innerText = remainingBalance;
+
     } else {
-        alert('Your balance is low')
-        saveAmountText.innerText = '00';
-        remainingBalanceText.innerText = '00';
+        saveAmountText.innerText = "You don't have sufficient balance for saving.";
+        remainingBalanceText.innerText = balanceTotalValue;
     }
 
 })
